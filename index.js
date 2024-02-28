bbcdec6d423c261565117135590f52e15028ce80
hemanth7301 <saihemanthsakhamuri@gmail.com>
2024-02-28 18:54:53 +0530

    third commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Hemanth Sakhamuri porfolio is coming soon");
+  res.send("Hemanth Sakhamuri portfolio is coming soon");
 });
 
 app.listen(3000);
